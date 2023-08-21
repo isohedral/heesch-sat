4600315ba62fdd8474a210bd9e96ef901667f16f
Craig S. Kaplan <azriel@gmail.com>
2023-08-21 17:44:27 -0400

    viz command line

--- src/viz.cpp
+++ src/viz.cpp
@@ -14,14 +14,59 @@ static cairo_surface_t *pdf;
 static cairo_t *cr;
 
 // Draw 6x8 grids of shapes per page.
-size_t grid_y = 0;
-size_t grid_x = 0;
+static size_t grid_y = 0;
+static size_t grid_x = 0;
+
+static bool draw_all = true;
+static bool draw_unknown = false;
+static bool draw_holes = false;
+static bool draw_inconclusive = false;
+static bool draw_nontiler = false;
+static bool draw_isohedral = false;
+static bool draw_all_heesch = true;
+static std::unordered_set<size_t> hcs;
+static std::unordered_set<size_t> hhs;
 
 using namespace std;
 
 template<typename grid>
+static bool shouldDraw( const TileInfo<grid>& tile )
+{
+	using info_t = TileInfo<grid>;
+	switch( tile.getRecordType() ) {
+		case info_t::UNKNOWN:
+			return draw_all || draw_unknown;
+		case info_t::HOLE:
+			return draw_all || draw_holes;
+		case info_t::INCONCLUSIVE:
+			return draw_all || draw_inconclusive;
+		case info_t::NONTILER:
+			if( draw_all || draw_nontiler ) {
+				if( draw_all_heesch ) {
+					return true;
+				}
+				if( hcs.find( tile.getHeeschConnected() ) != hcs.end() ) {
+					return true;
+				}
+				if( hhs.find( tile.getHeeschHoles() ) != hhs.end() ) {
+					return true;
+				}
+			}
+			return false;
+		case info_t::ISOHEDRAL:
+			return draw_all || draw_isohedral;
+		default:
+			return draw_all;
+		}
+}
+
+template<typename grid>
 static bool drawPatch( const TileInfo<grid>& tile )
 {
+	if( !shouldDraw( tile ) ) {
+		return true;
+	}
+
     Visualizer<grid> viz { cr, tile };
 	viz.setColourByOrientation( ori_col );
 
@@ -92,12 +137,45 @@ int main( int argc, char **argv )
 		} else if( !strcmp( argv[idx], "-o" ) ) {
 			++idx;
 			outname = argv[idx];
+		} else if( !strcmp( argv[idx], "-unknown" ) ) {
+			draw_unknown = true;
+		} else if( !strcmp( argv[idx], "-holes" ) ) {
+			draw_holes = true;
+		} else if( !strcmp( argv[idx], "-inconclusive" ) ) {
+			draw_inconclusive = true;
+		} else if( !strcmp( argv[idx], "-nontiler" ) ) {
+			draw_nontiler = true;
+		} else if( !strcmp( argv[idx], "-isohedral" ) ) {
+			draw_isohedral = true;
+		} else if( !strcmp( argv[idx], "-hcs" ) ) {
+			draw_all_heesch = false;
+			++idx;
+			IntReader i { argv[idx] };
+			IntReader iend { argv[idx] + strlen( argv[idx] ) };
+			while( i != iend ) {
+				hcs.insert( *i );
+				++i;
+			}
+		} else if( !strcmp( argv[idx], "-hhs" ) ) {
+			draw_all_heesch = false;
+			++idx;
+			IntReader i { argv[idx] };
+			IntReader iend { argv[idx] + strlen( argv[idx] ) };
+			while( i != iend ) {
+				hhs.insert( *i );
+				++i;
+			}
 		} else {
 			cerr << "Unrecognized parameter \"" << argv[idx] << "\""
 				<< endl;
 			exit( 0 );
 		}
     }
+
+	if( draw_unknown || draw_holes || draw_inconclusive || draw_nontiler
+			|| draw_isohedral ) {
+		draw_all = false;
+	}
 
 	pdf = cairo_pdf_surface_create( outname, 8.5*72, 11*72 );
 	cr = cairo_create( pdf );
